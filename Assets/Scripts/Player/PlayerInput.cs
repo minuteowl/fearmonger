8d1662c09e58006a2cf878ce18300d2ab39339ed
Michael Davis <davism4@umbc.edu>
2014-02-10 21:43:34 -0500

    Added some new scripts that need to be implemented. Also set up object-room position game object prefabs, which will be used for placing things in the rooms via scripts.

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PlayerInput {
+
+	// Functions to normalize keyboard input
+	public static bool InputRight()
+	{
+		return (Input.GetKey("right") && !Input.GetKey("left"));
+	}
+
+	public static bool InputLeft()
+	{
+		return (Input.GetKey("left") && !Input.GetKey("right"));
+	}
+
+	public static bool InputUp()
+	{
+		return (Input.GetKey("up") && !Input.GetKey("down"));
+	}
+
+	public static bool InputDown()
+	{
+		return (Input.GetKey("down") && !Input.GetKey("up"));
+	}
+
+	public static bool InputActivate()
+	{
+		return (Input.GetKey("v") || Input.GetKey("V") || Input.GetKey ("space"));
+	}
+
+	public static bool InputInvisible()
+	{
+		return (Input.GetKey("c") || Input.GetKey("C"));
+	}
+
+	public static bool InputMap()
+	{
+		return (Input.GetKey("x") && !Input.GetKey("z") && !Input.GetKey ("enter"));
+	}
+
+	public static bool InputMenu()
+	{
+		return ((Input.GetKey("z")||Input.GetKey ("enter")) && !Input.GetKey("x"));
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Vector2 facingDirection;
+
+	Vector2 xAxis, yAxis;
+
+	public bool IsInvisible = true;
+
+	// Use this for initialization
+	void Start () {
+		xAxis = new Vector2(1,0);
+		yAxis = new Vector2(0,1);
+	}
+
+	void EnterRoom()
+	{
+		IsInvisible = true;
+		facingDirection = yAxis;
+	}
+
+	void ToggleInvisible()
+	{
+		IsInvisible = !IsInvisible;
+	}
+
+	void Move()
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (PlayerInput.InputMenu()){
+		}
+		else if (PlayerInput.InputMap())
+		{
+		}
+		else if (PlayerInput.InputInvisible())
+		{
+			ToggleInvisible();
+		}
+		else
+		{
+			if (PlayerInput.InputRight())
+			{
+				facingDirection = xAxis;
+				Move ();
+			}
+			else if (PlayerInput.InputLeft())
+			{
+				facingDirection = -xAxis;
+				Move ();
+			}
+			if (PlayerInput.InputUp())
+			{
+				facingDirection = yAxis;
+				Move ();
+			}
+			else if (PlayerInput.InputDown())
+			{
+				facingDirection = -yAxis;
+				Move ();
+			}
+
+		}
+	}
+}
