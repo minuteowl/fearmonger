8f4df1b52b4801ee0ec5fe269a065ec6e9e0b246
Steve Hart <harste1@umbc.edu>
2014-03-12 00:06:59 -0400

    Spider power particle emitter created

---
+++ Assets/Scripts/Ability/Ability_Spiders.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability_Spiders : Ability {
+	
+	// Use this for initialization
+	public Ability_Spiders () {
+		Name="Spawn Spiders";
+		Description = "Spiders come out of the ground at a specific point.";
+		MinLevel=1;
+		Duration = 2.5f;
+		EnergyCost = 2;
+		BuyCost = 1;
+	}
+	
+	public override void UseAbility (PlayerLevel level, MonoBehaviour[] args) {
+		Debug.Log("spider spell");
+		// create a tentacles object - to be implemented later
+	}
+}
