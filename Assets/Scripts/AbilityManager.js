662c7aea286de9df2052fd243f51e124623fc7e4
Steve Hart <harste1@umbc.edu>
2014-03-14 22:22:56 -0400

    Added preliminary 3d guy elements for spell activation

---
+++ Assets/Scripts/AbilityManager.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+static public ability_spider = false;
+var spiderAbility : GameObject
+		
+    	 
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+//on click --> if any powers are active, put the object there
+function onClick(){
+
+	if(ability_spider){
+		var x = Camera.main.ScreenToWorldPoint(Input.mousePosition.x);
+		var y = Camera.main.ScreenToWorldPoint(Input.mousePosition.y);
+		var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
+
+	}
+}
