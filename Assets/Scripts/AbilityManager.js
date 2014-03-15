ffb2e653ce6cedc6afff2d7adcb071777768b423
Steve Hart <harste1@umbc.edu>
2014-03-15 03:32:25 -0400

    Steve - added spider ability and GUI spell activator on left.  Spell is Toggle on/off and creates the spell effect at the mouse location clicked when active.  No effect has been put on the user since there are no users as of yet.

--- Assets/Scripts/AbilityManager.js
+++ Assets/Scripts/AbilityManager.js
@@ -1,25 +1,28 @@
 ﻿#pragma strict
 
-static public ability_spider = false;
-var spiderAbility : GameObject
+//ability variables
+static var ability_spider = true;
+var spiderAbility : GameObject;
 		
-    	 
-function Start () {
 
-}
 
 function Update () {
 
-}
-
-
-//on click --> if any powers are active, put the object there
-function onClick(){
-
-	if(ability_spider){
-		var x = Camera.main.ScreenToWorldPoint(Input.mousePosition.x);
-		var y = Camera.main.ScreenToWorldPoint(Input.mousePosition.y);
-		var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
-
+	//when the user clicks
+	if(Input.GetMouseButtonDown(0)){
+		Debug.Log("Pressed left click.");
+		//find click location
+			var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+    		var point : Vector3 = ray.origin + (ray.direction);    
+			var x = point.x;
+			var y = point.y;
+		//check for active spider ability
+		if(ability_spider){
+			//create spider spell at mouse location
+			var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
+			//destroy spell after completion
+			Destroy (spider, 10.0); 
+		}
 	}
+
 }
