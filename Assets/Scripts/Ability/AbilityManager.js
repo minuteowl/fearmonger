03f510b4d6d284def954f755384d7ee529c86e78
Steve Hart <harste1@umbc.edu>
2014-03-26 11:03:58 -0400

    Updated spells to only work within the room

--- Assets/Scripts/Ability/AbilityManager.js
+++ Assets/Scripts/Ability/AbilityManager.js
@@ -17,15 +17,25 @@ function Update () {
     		var point : Vector3 = ray.origin + (ray.direction);    
 			var x = point.x;
 			var y = point.y;
-		//---------------------------SPIDER ABILITY
-		if(ability_spider){
-			//create spider spell at mouse location
-			var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
-			//push all characters away
 			
-			//destroy spell after completion
-			Destroy (spider, 10.0); 
+		//determine if cursor is inside room
+		var mousePos = Input.mousePosition;
+		mousePos.z = 1.0;
+		var pz = Camera.main.ScreenToWorldPoint(mousePos);
+		if (pz.x > -10 && pz.x < 10){
+		
+			//---------------------------SPIDER ABILITY
+			if(ability_spider){
+				//create spider spell at mouse location
+				var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
+				//push all characters away
+				
+				//destroy spell after completion
+				Destroy (spider, 10.0); 
+				}
+		}
+		else{
+			Debug.Log("Pressed left click.");	
 		}
 	}
-
 }
