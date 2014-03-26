5b8270481332327da4525af07ea260150a4feffc
Steve Hart <harste1@umbc.edu>
2014-03-26 10:46:20 -0400

    updated Spider spell code and particle emitter with colored sprites that age over time and shrink

--- Assets/Scripts/Ability/AbilityManager.js
+++ Assets/Scripts/Ability/AbilityManager.js
@@ -17,10 +17,12 @@ function Update () {
     		var point : Vector3 = ray.origin + (ray.direction);    
 			var x = point.x;
 			var y = point.y;
-		//check for active spider ability
+		//---------------------------SPIDER ABILITY
 		if(ability_spider){
 			//create spider spell at mouse location
 			var spider = Instantiate(spiderAbility, new Vector3(x,y,-1), Quaternion.identity) as GameObject;
+			//push all characters away
+			
 			//destroy spell after completion
 			Destroy (spider, 10.0); 
 		}
